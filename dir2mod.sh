da80c8637d80d62052270866f9ed10a6e569a637
kewang <cpckewang@gmail.com>
2013-07-15 14:55:04 +0800

    refine dir2mod.sh

--- dir2mod.sh
+++ dir2mod.sh
@@ -8,7 +8,7 @@
 read -p "Please input original repository folder: " ORIG_REPO_FOLDER
 read -p "Please input new submodule name: " NEW_MOD_NAME
 read -p "Please input want to change folder name: " DIR_NAME
-read -p "Please input new submodule repository name: " NEW_MOD_REPO_NAME
+read -p "Please input new submodule repository url: " NEW_MOD_REPO_URL
 
 # Clone new repositories.
 git clone --no-hardlinks $ORIG_REPO_FOLDER $NEW_MOD_NAME
@@ -22,15 +22,16 @@ git prune
 git remote rm origin
 
 # Push the new repositories to the upstream server.
-git remote add origin $NEW_MOD_REPO_NAME
+git remote add origin $NEW_MOD_REPO_URL
 git push -u origin master
 
 # Add the new repository as submodules to the original repository
 cd ../$ORIG_REPO_FOLDER
 git rm -r $DIR_NAME
 git commit -m "Removing the folders that are now repositories"
-git submodule add $NEW_MOD_REPO_NAME $DIR_NAME
+git submodule add $NEW_MOD_REPO_URL $DIR_NAME
 git submodule init
 git submodule update
 git add .
 git commit -m "Added in submodules for removed folders"
+git push
